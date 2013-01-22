3beb56b2cbb476f0e1d37faaf2e72c98f163e6aa
David Burken <dburken@comcast.net>
2013-01-22 14:39:56 -0500

    Added comments on how to run.

--- deleteRepo.groovy
+++ deleteRepo.groovy
@@ -1,3 +1,11 @@
+//---
+// File: deleteRepo.groovy
+// To run do:
+// Edit below repository name.
+// $ cd $OMAR_HOME
+// $ grails prod run-script /work/test/omar-scripts/deleteRepo.groovy
+//---
+
 import org.ossim.omar.core.Repository
 import org.ossim.omar.raster.RasterDataSet
 
@@ -5,7 +13,8 @@ println Repository.list().baseDir
 
 Repository.withTransaction {
 
-    def repository = Repository.findByBaseDir('/data/celtic' )
+    // Edit below line with the repository you want to delete:
+    def repository = Repository.findByBaseDir('/data3/doconnor/image_mart1' )
     
     def results = RasterDataSet.createCriteria().scroll() {
         eq('repository', repository)
